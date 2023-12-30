fa87b7959dc70a6561ce8c6af203bce0ef4e9304
Md.Rabiul Hasan <mdrabiulhasan.me@gmail.com>
2023-12-30 19:02:02 +0600

    accepting request

---
+++ src/main/java/com/rabiul/quizapp/QuestionController.java
@@ -0,0 +1,14 @@
+package com.rabiul.quizapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+    @GetMapping("all-question")
+    public String getAllQuestion(){
+        return "Hello World";
+    }
+}
